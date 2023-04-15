60dd35ca1a35ae7fa96a0d46e4a6e84a1ad1494d
khanhtoanmeo <115925844+khanhtoanmeo@users.noreply.github.com>
2023-04-15 20:58:30 +0700

    add insertion sort

--- sort.js
+++ sort.js
@@ -1,5 +1,3 @@
-const arr = [4, 6, 1, 8, 2, 10, 26, 4, 100, 65, 88];
-
 function swap(arr, i, j) {
   let temp = arr[i];
   arr[i] = arr[j];
@@ -33,7 +31,21 @@ class Sort {
     }
     return arr;
   }
+  static insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      let cur = arr[i];
+      let curJ = i;
+      for (let j = i - 1; j >= 0 && arr[j] > cur; j--) {
+        arr[j + 1] = arr[j];
+        curJ = j;
+      }
+      arr[curJ] = cur;
+    }
+    return arr;
+  }
 }
 
-console.log(Sort.selectionSort(arr));
-console.log(Sort.bubbleSort(arr));
+const arr = [4, 6, 1, 8, 2, 10, 26, 4, 100, 65, 88];
+// console.log(Sort.selectionSort(arr));
+// console.log(Sort.bubbleSort(arr));
+console.log(Sort.insertionSort(arr));
