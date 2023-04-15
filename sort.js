6b2e7cfc0e5d3943a2f7b89eb7d969da35be5a3a
khanhtoanmeo <115925844+khanhtoanmeo@users.noreply.github.com>
2023-04-15 19:00:09 +0700

    add selection sort

--- sort.js
+++ sort.js
@@ -1,5 +1,11 @@
 const arr = [4, 6, 1, 8, 2, 10, 26, 4, 100, 65, 88];
 
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
 class Sort {
   static bubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
@@ -7,9 +13,7 @@ class Sort {
       let t = 0;
       for (let j = 0; j < arr.length - i - 1; j++) {
         if (arr[j] > arr[j + 1]) {
-          let temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
+          swap(arr, j, j + 1);
           noswaps = false;
         }
         t += 1;
@@ -18,6 +22,18 @@ class Sort {
     }
     return arr;
   }
+
+  static selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+          swap(arr, i, j);
+        }
+      }
+    }
+    return arr;
+  }
 }
 
+console.log(Sort.selectionSort(arr));
 console.log(Sort.bubbleSort(arr));
